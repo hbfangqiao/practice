83e3663f193921424324595029d820931d228203
fq <hbfangqiao@163.com>
2019-09-07 22:55:47 +0800

    update hourse and add a person servcie

--- src/entity/Hourse.java
+++ src/entity/Hourse.java
@@ -5,4 +5,13 @@ package entity;
  * @create 2019-09-07
  */
 public class Hourse {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
---
+++ src/service/PersonService.java
@@ -0,0 +1,8 @@
+package service;
+
+/**
+ * @author fq
+ * @create 2019-09-07
+ */
+public class PersonService {
+}
